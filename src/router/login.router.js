4315fef18f5bdfadc56800f07740fa7d40f51eca
zhaipengju <3045908145@qq.com>
2025-04-18 21:48:07 +0800

    fix:删除无用代码

--- src/router/login.router.js
+++ src/router/login.router.js
@@ -1,6 +1,6 @@
 const KoaRouter = require("@koa/router")
-const { sign, test } = require("../controller/login.controller")
-const { verifyLogin, verifyAuth } = require("../middleware/login.middleware")
+const { sign } = require("../controller/login.controller")
+const { verifyLogin } = require("../middleware/login.middleware")
 const loginRouter = new KoaRouter({ prefix: '/login' })
 
 loginRouter.post("/", verifyLogin, sign)
