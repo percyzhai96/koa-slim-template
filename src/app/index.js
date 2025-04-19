d866fe46587705113b9bb7239dcbfd4d4364dc40
zhaipengju <3045908145@qq.com>
2025-04-19 11:16:10 +0800

    fix:submit

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("koa-bodyparser")
 const registerRoutes = require("../router/index")
 
 const app = new Koa()
-registerRoutes(app) // 自动化注册路由
 app.use(bodyParser())
+registerRoutes(app) // 自动化注册路由
 
 module.exports = app
