91d9e45ba1e4b01741f0910c4fa54b946c4516f4
zhaipengju <3045908145@qq.com>
2025-04-20 17:39:22 +0800

    fix:接口优化

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -12,8 +12,9 @@ class MomentService {
     const statement = `
     SELECT
     m.id AS id, m.content AS content,m.createAt AS createTime, m.updateAt AS updateTime,
-    JSON_OBJECT('id',u.id,'name',u.name,'createTime',u.createAt,'updateTime',u.updateAt) AS user
-    FROM moment AS m LEFT JOIN user AS u ON  u.id = m.user_id LIMIT ? OFFSET ?;
+    JSON_OBJECT('id',u.id,'name',u.name,'createTime',u.createAt,'updateTime',u.updateAt) AS user,
+    (SELECT COUNT(*) FROM comment WHERE comment.moment_id = m.id) AS commentCount
+    FROM moment AS m LEFT JOIN user AS u ON  u.id = m.user_id LIMIT 10 OFFSET 0;
     `
     const result = await connection.execute(statement, [String(size), String(offset)])
     return result
@@ -24,8 +25,16 @@ class MomentService {
     const statement = `
     SELECT
     m.id AS id, m.content AS content,m.createAt AS createTime, m.updateAt AS updateTime,
-    JSON_OBJECT('id',u.id,'name',u.name,'createTime',u.createAt,'updateTime',u.updateAt) AS user
-    FROM moment AS m LEFT JOIN user AS u ON  u.id = m.user_id WHERE m.id = ?;
+    JSON_OBJECT('id',u.id,'name',u.name,'createTime',u.createAt,'updateTime',u.updateAt) AS user,
+    (JSON_ARRAYAGG(JSON_OBJECT(
+      'id',c.id,'content',c.content,'commentId',c.comment_id,'user',JSON_OBJECT('id',cu.id,'name',cu.name)
+    ))) comments 
+    FROM moment AS m 
+    LEFT JOIN user AS u ON  u.id = m.user_id 
+    LEFT JOIN comment AS c ON c.comment_id = m.id
+    LEFT JOIN user cu ON c.user_id
+    WHERE m.id = 2
+    GROUP BY c.moment_id;
     `
     const [result] = await connection.execute(statement, [momentId])
     return result
